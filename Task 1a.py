2fd1e5cd97a49a6cf0fda2c518481bcb56d1f51f
Paulina Wacławik-Bagińska <paulina.waclawik@gmail.com>
2025-12-24 01:44:03 +0100

    Add Task 1a.py

---
+++ Task 1a.py
@@ -0,0 +1,10 @@
+names = ["Anna", "Marek", "Beata", "Artur", "Edmund"]
+
+
+def show_names(names_list):
+    """Wyświetla każde imię z przekazanej listy."""
+    for name in names_list:
+        print(name)
+
+
+show_names(names)
